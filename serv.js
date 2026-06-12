88465f11b77f5380eedecdd36c65600d7777f7e3
zecogit <zekicicek@yandex.com>
2026-06-12 20:15:05 +0300

    Implement WebSocket server on port 8080
    Added WebSocket server functionality to serv.js.

--- serv.js
+++ serv.js
@@ -4,3 +4,11 @@ http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('Hello, World!\n');
 }).listen(3000);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+   console.log('New client connected');
+   // Sending a message to the client
+   ws.send('Welcome to the WebSocket server!');
+   // Listening
